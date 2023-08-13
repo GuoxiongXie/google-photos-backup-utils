7f203e65b373afb98c8f58da7dca0e24895fae3d
GuoxiongXie <jake.g.xie@gmail.com>
2023-08-13 13:57:05 -0700

    added logic to avoid duplicate file name in file renaming tool

--- FileRenamingTool.py
+++ FileRenamingTool.py
@@ -1,6 +1,7 @@
 import os
 from time import time
 from random import random
+from math import floor
 
 """
 Some photo service has character limits in file name and rejects long names.
@@ -9,6 +10,8 @@ that's > # of files in directory to avoid dup and generate a relatively short na
 File names will be changed in place.
 Note that it doesn't take multiple dots in the filename in account. 
 i.e. file.name.extension will be renamed to randomNumber.extension
+
+If there's duplicate with the randomly generated name, append __{count} to the end of file name (before extension)
 """
 
 DIR_SIZE = 10_000 # better use a number >>> # of files in directory
@@ -16,11 +19,15 @@ DIR_SIZE = 10_000 # better use a number >>> # of files in directory
 def main() -> None:
     directory = input("Enter source directory path (files will be renamed in place): ")
     start_time = time()
+    seen_before = set()
 
     for file in os.listdir(directory):
         source_file_name, extension = os.path.splitext(file)
-        source_file_name = random() * DIR_SIZE
-        destination_file_name = f"{source_file_name}{extension}"
+        randomized_file_name = floor(random() * DIR_SIZE)
+        while randomized_file_name in seen_before:  # keep generating random name until it hasn't seen before
+            randomized_file_name = floor(random() * DIR_SIZE)
+        seen_before.add(randomized_file_name)
+        destination_file_name = f"{randomized_file_name}{extension}"
 
         source_file = os.path.join(directory, file)
         destination_file = os.path.join(directory, destination_file_name)
